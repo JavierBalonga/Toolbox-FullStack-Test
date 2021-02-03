141bb384de58e45a577269422689c3b9166e4314
JavierBalonga <56722632+JavierBalonga@users.noreply.github.com>
2021-02-03 14:13:18 -0300

    feat(client/src/store/*)redux action addResult --> iecho request

--- client/src/store/actionCreators.js
+++ client/src/store/actionCreators.js
@@ -1,1 +1,10 @@
+import { iecho } from '../services/api'
 
+export const ADD_RESULT = 'ADD_RESULT'
+export function addResult (text) {
+  return function (dispatch) {
+    return iecho(text).then(({ data }) => {
+      dispatch({ type: ADD_RESULT, payload: data })
+    })
+  }
+}
--- client/src/store/reducer.js
+++ client/src/store/reducer.js
@@ -1,12 +1,18 @@
-import { } from './actionCreators'
+import { ADD_RESULT } from './actionCreators'
 
 const initialState = {
-  // ...
+  results: [],
+  resultsId: 1
 }
 
 export default function reducer (state = initialState, { type, payload }) {
   switch (type) {
-    // ...
+    case ADD_RESULT:
+      return {
+        ...state,
+        results: state.results.concat({ ...payload, id: state.resultsId }),
+        resultsId: state.resultsId + 1
+      }
     default:
       return state
   }
